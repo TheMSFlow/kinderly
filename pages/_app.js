3045e763cfd0c8f59c1844cf5f85c8f18dc35aca
Michael <themichaelsteve@gmail.com>
2025-06-02 20:00:53 +0100

    installation complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
 
+import { Geist } from "next/font/google";
+import { Playfair_Display } from "next/font/google";
+
+
+const geist = Geist({ subsets: ["latin"] });
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${geist.className} ${playfair.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
